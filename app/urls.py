c67d351373631a2bfd6e003e2df18bca5f607247
Rishi_Goyal <rishi1798goyal@gmail.com>
2023-04-10 10:59:05 +0530

    login and signup token authentication implemented

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
 from rest_framework.authtoken.views import obtain_auth_token
-
+from app.views import registration_view
 
 urlpatterns = [
     path("login/",obtain_auth_token,name="login"),
+    path("register/",registration_view,name="register"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from app.serializers import RegistrationSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+# from app.models import create_auth_token
+from rest_framework.authtoken.models import Token
 # Create your views here.
+
+
+
+@api_view(['POST'])
+def registration_view(request):
+
+    if request.method == "POST":
+
+        data = {}
+           
+        serializer=RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            account=serializer.save()
+            print(account)
+            data['response']='Registration Successful'
+            data['username']=account.username
+            data['eamil']=account.email
+
+            token= Token.objects.get_or_create(user=account)
+            data['Token'] = str(token)
+            
+        else:
+            serializer.errors
+        return Response(data)
+
